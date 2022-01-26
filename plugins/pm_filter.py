6ba822af61fcdb76db42eb7d81d52b291d8b8c1f
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2022-01-26 12:15:38 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -26,7 +26,7 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 SPELL_CHECK = {}
 
-@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
+@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming & ~filters.command(["song"]))
 async def give_filter(client,message):
     group_id = message.chat.id
     name = message.text
